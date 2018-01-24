49557760306ff01d2746a6580454fbf1e5150500
shadowsyntax <tibetianwolf@gmail.com>
2018-01-24 21:53:41 +0100

    Updated the upload build script for AWS lambda

--- upload_valise_lambda.py
+++ upload_valise_lambda.py
@@ -3,17 +3,22 @@ import zipfile
 import StringIO
 import mimetypes
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
 
-valise_bucket = s3.Bucket('valise.sherifolanrewaju.be')
-build_bucket = s3.Bucket('valisebuild.sherifolanrewaju.be')
+    valise_bucket = s3.Bucket('valise.sherifolanrewaju.be')
+    build_bucket = s3.Bucket('valisebuild.sherifolanrewaju.be')
 
-valise_zip = StringIO.StringIO()
-build_bucket.download_fileobj('valisebuild.zip', valise_zip)
+    valise_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('valisebuild.zip', valise_zip)
 
-with zipfile.ZipFile(valise_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        valise_bucket.upload_fileobj(obj, nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        valise_bucket.Object(nm).Acl().put(ACL='public-read')
+    with zipfile.ZipFile(valise_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            valise_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            valise_bucket.Object(nm).Acl().put(ACL='public-read')
+            
+    print 'Job done!'
+            
+    return 'Hello from Lambda'
